c921018362b9c217750c1f40adb769dde1758e71
bstrzele <bstrzelecki25@gmail.com>
2025-05-31 18:38:40 +0200

    initial hybrid

--- segmentation/configs/_base_/models/upernet_r50.py
+++ segmentation/configs/_base_/models/upernet_r50.py
@@ -1,5 +1,5 @@
 # model settings
-norm_cfg = dict(type='SyncBN', requires_grad=True)
+norm_cfg = dict(type='BN', requires_grad=True)
 model = dict(
     type='EncoderDecoder',
     pretrained='open-mmlab://resnet50_v1c',
@@ -8,8 +8,8 @@ model = dict(
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        dilations=(1, 1, 1, 1),
-        strides=(1, 2, 2, 2),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
         norm_cfg=norm_cfg,
         norm_eval=False,
         style='pytorch',
